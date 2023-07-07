6547565527be932d9f936b114f1e0d03579c69e9
huydsai02 <huydsai02@gmail.com>
2023-07-07 13:35:38 +0700

    update

--- backend_api/hello.py
+++ backend_api/hello.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 import uvicorn, numpy as np
-import random, cv2
+import random, cv2, os
 from io import BytesIO
 import base64, time
 from PIL import Image
@@ -19,20 +19,23 @@ def stringToRGB(base64_string):
 
 
 count = [0]
-
+current_folder = os.path.dirname(os.path.realpath(__file__))
 @app.post("/movie_controller")
 async def detect(request: Request):
     json_file: bytes = await request.json()
     imageSrc = json_file['imageSrc']
     image_np = stringToRGB(imageSrc)
-    cv2.imwrite("test.png", image_np)
+    img_path = os.path.join(current_folder, "img.png")
+    if os.path.exists(img_path):
+        os.remove(img_path)
+    cv2.imwrite(img_path, image_np)
     i = 101
     count[0] += 1
     if (count[0] % 10 == 0):
         i = 5
     # count[0] = 100
-    print(f"SEND DATA {i}")
-    return {"content": f"{i}"}
+    # print(f"SEND DATA {'mute'}")
+    return {"content": f"100"}
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
